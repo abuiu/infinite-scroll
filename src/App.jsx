1b119f38186791b7978620157f5f362557a230c1
Andrei Buiu <andrei@buiu.co.uk>
2022-01-04 12:08:18 +0200

    first blood

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './App.css';
+import Header from './component/Header';
+import axios from 'axios';
+import UsersList from './component/UsersList';
+
+class App extends React.Component {
+
+  state = {
+    users: [],
+    page: 0,
+    isLoading: false,
+    errorMsg: ''
+  };
+
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      this.loadUsers();
+    }
+  }
+  loadUsers = () => {
+    const { page } = this.state;
+
+    this.setState({ isLoading: true });
+    axios
+      .get(`https://randomuser.me/api/?page=${page}&results=10`)
+      .then((response) => {
+        this.setState((prevState) => ({
+          users: [...prevState.users, ...response.data.results],
+          errorMsg: ''
+        }));
+      })
+      .catch((error) =>
+        this.setState({
+          errorMsg: 'Error while loading data. Try again later.'
+        })
+      )
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  };
+
+  loadMore = () => {
+    this.setState((prevState) => ({
+      page: prevState.page + 1
+    }));
+  };
+
+  render() {
+    const { users, isLoading, errorMsg } = this.state;
+  
+    return (
+      <div className="main-section">
+        <Header />
+        <UsersList users={users} />
+        {errorMsg && <p className="errorMsg">{errorMsg}</p>}
+        <div className="load-more">
+          <button onClick={this.loadMore} className="btn-grad">
+            {isLoading ? 'Loading...' : 'Load More'}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
